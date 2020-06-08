d819addd214e1c68aa7cdd6449fac0e68c94e88e
Akane <soranyuxe@gmail.com>
2020-06-08 17:24:51 +0200

    Auto stash before merge of "master" and "origin/master"

--- iot_digitalTwins/src/Controller/IndexController.php
+++ iot_digitalTwins/src/Controller/IndexController.php
@@ -40,7 +40,7 @@ class IndexController extends AbstractController
         
         $mqtt = new phpMQTT($server, $port, $client_id);
         
-        $temp=["temp" => 50];
+        $temp=["temp" => 100];
 
         if ($mqtt->connect(true, NULL, $username, $password)) {
             //$mqtt->publish('iot-2/evt/temp/fmt/json', json_encode($temp), 0, false);
@@ -52,32 +52,36 @@ class IndexController extends AbstractController
             $message= "Time out!";
         }
 
-        // $mqtt = new phpMQTT($server, $port, $client_id);
-        // if(!$mqtt->connect(true, NULL, $username, $password)) {
-        //     exit(1);
-        // }
-        
-        // $mqtt->debug = true;
-        
-        // $topics['iot-2/type/DTC/id/Akane/evt/temp/fmt/json'] = array('qos' => 0, 'function' => 'procMsg');
-        // $mqtt->subscribe($topics, 0);
-        
-        // while($mqtt->proc()) {
-        
-        // }
-        
-        // $mqtt->close();
-        
-        // function procMsg($topic, $msg){
-        //         echo 'Msg Recieved: ' . date('r') . "\n";
-        //         echo "Topic: {$topic}\n\n";
-        //         echo "\t$msg\n\n";
-        // }
-
-        return $this->render('index/home.html.twig', [
+        return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'message' => $message,
             'temp' => json_encode($temp),
+        ]);
+    }
+
+
+    /**
+     * @Route("/subscribe", name="subscribe")
+     */
+    public function subscribe()
+    {
+        $server = '1nygvs.messaging.internetofthings.ibmcloud.com';     // change if necessary
+        $port = 1883;                     // change if necessary
+        $username = 'a-1nygvs-avu0otj3wv';                   // set your username
+        $password = 'sfT02PH1sJn0lsSZcL';                   // set your password
+        $client_id = 'a:1nygvs:avu0otj3wv'; // make sure this is unique for connecting to sever - you could use uniqid()
+        
+        $mqtt = new phpMQTT($server, $port, $client_id);
+        if(!$mqtt->connect(true, NULL, $username, $password)) {
+            exit(1);
+        }
+        
+        echo $mqtt->subscribeAndWaitForMessage('iot-2/type/DTC/id/Akane/evt/temp/fmt/json', 0);
+        
+        $mqtt->close();
+
+        return $this->render('index/home.html.twig', [
+            'controller_name' => 'IndexController',
         ]);
     }
     
